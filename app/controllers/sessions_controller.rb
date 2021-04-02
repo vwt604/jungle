9fc3ff72e862a01cab053105c9868c53d895f4eb
Vanessa Y <vyeung.wt@gmail.com>
2021-04-02 14:46:11 +0000

    fix session controllers logic to login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,14 @@
 class SessionsController < ApplicationController
-  def new
+  def create
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      #Save the user id inside the browser cookie.
+      # Save the user id inside the browser cookie.
       session[:user_id] = user.id
       redirect_to '/'
+    else
+      redirect_to '/login'
     end
-  end
-
-  def create
-    redirect_to '/login'
   end
 
   def destroy
