d9b80d3167cb04280b754542b0e26674a2bdb2be
Vanessa Y <vyeung.wt@gmail.com>
2021-04-02 14:38:34 +0000

    add user authen to categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,9 @@
 class CategoriesController < ApplicationController
 
+  before_filter :authorize
+
   def show
     @category = Category.find(params[:id])
-    # raise @category.inspect
     @products = @category.products.order(created_at: :desc)
   end
 
