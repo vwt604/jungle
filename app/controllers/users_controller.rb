4c667a4c1237e6f993284255c1d97dbdec7862e9
Vanessa Y <vyeung.wt@gmail.com>
2021-04-04 01:47:03 +0000

    before merge to master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to :products, notice: 'User created!'
+      redirect_to '/', notice: 'User created!'
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   #give User model bcrypt authentication methods
   has_secure_password
+
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
+  
 end
