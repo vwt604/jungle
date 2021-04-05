a17d61e35ab55de95a1b6b618c04abf54c40a500
Vanessa Y <vyeung.wt@gmail.com>
2021-04-05 22:33:32 +0000

    add product price validation test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,6 +14,15 @@ RSpec.describe Product, type: :model do
       category.save!
       product = Product.new(price: 10, quantity: 10, category_id: category.id)
       expect(product).to_not be_valid
+      expect(product.errors[:name]).to(include("can't be blank"))
+    end
+
+    it 'should be invalid if price is missing' do
+      category = Category.new()
+      category.save!
+      product = Product.new(name: "Funky Hat", quantity: 10, category_id: category.id)
+      expect(product).to_not be_valid
+      expect(product.errors[:price]).to(include("can't be blank"))
     end
 
   end
