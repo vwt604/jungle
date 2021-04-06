b7f725e5518cc829b5fcd5fb844e42addb77ec7b
Vanessa Y <vyeung.wt@gmail.com>
2021-04-06 00:13:42 +0000

    add user creation validation test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it 'should be valid when given all four fields' do
+    it 'should be valid when given all fields' do
       category = Category.new()
       category.save!
       product = Product.new(name: "Funky Hat", price: 10, quantity: 10, category_id: category.id)
@@ -22,7 +22,7 @@ RSpec.describe Product, type: :model do
       category.save!
       product = Product.new(name: "Funky Hat", quantity: 10, category_id: category.id)
       expect(product).to_not be_valid
-      expect(product.errors[:price]).to(include("can't be blank"))
+      expect(product.errors[:price]).to(include("is not a number"))
     end
 
     it 'should be invalid if quantity is missing' do
@@ -40,6 +40,8 @@ RSpec.describe Product, type: :model do
       expect(product).to_not be_valid
       expect(product.errors[:category]).to(include("can't be blank"))
     end
-    
+
   end
 end
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'should be valid when all fields are rentered correctly' do
+      user = User.new(email: 'test@example.com', first_name: 'Joe', last_name: 'Doe', password: 'password', password_confirmation: 'password')
+      expect(user).to be_valid
+    end
+
+    it 'should be invalid if email is missing' do
+      user = User.new(first_name: 'Joe', last_name: 'Doe', password: 'password', password_confirmation: 'password')
+      expect(user).to_not be_valid
+      expect(product.errors[:name]).to(include("can't be blank"))
+    end
 end
