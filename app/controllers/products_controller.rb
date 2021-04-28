5fe742156dfd580b61050f1451a890406a203e04
Vanessa Y <vyeung.wt@gmail.com>
2021-04-28 20:49:03 +0000

    comment cleanup

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    # uses MODEL
     @products = Product.all.order(created_at: :desc)
-    # render: :index (implicit)
-
-
   end
 
-  def show
-    # any of these @ instance variables will be available at render 'show' 
-    @product = Product.find params[:id]
-
-
-    
+  def show 
+    @product = Product.find params[:id]   
   end
 
 end
